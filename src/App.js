bd42dc830363b815abcc3fb51d0114c9f4034edf
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-04-16 20:02:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { getPlacesData, getWeatherData } from "./api";
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
 import Map from "./components/Map/Map";
+import { LoadScript } from "@react-google-maps/api";
 
 const App = () => {
     const [places, setPlaces] = useState([]);
@@ -17,11 +18,13 @@ const App = () => {
     const [rating, setRating] = useState(0);
     const [weatherData, setWeatherData] = useState([]);
     const [autocomplete, setAutocomplete] = useState(null);
+
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
             setCoordinates({ lat: latitude, lng: longitude });
         });
     }, []);
+
     useEffect(() => {
         setFilteredPlaces(places.filter((place) => place.rating > rating));
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -38,39 +41,44 @@ const App = () => {
         }
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [type, bounds]);
+
     const onLoad = (autoC) => setAutocomplete(autoC);
+
     const onPlaceChanged = () => {
         const lat = autocomplete.getPlace().geometry.location.lat();
         const lng = autocomplete.getPlace().geometry.location.lng();
         setCoordinates({ lat, lng });
     };
+
     return (
         <React.Fragment>
-            <CssBaseline />
-            <Header onLoad={onLoad} onPlaceChanged={onPlaceChanged} />
-            <Grid container spacing={3} style={{ width: '100%' }}>
-                <Grid item xs={12} md={4}>
-                    <List
-                        places={filteredPlaces.length ? filteredPlaces : places}
-                        childClicked={childClicked}
-                        isLoading={isLoading}
-                        type={type}
-                        setType={setType}
-                        rating={rating}
-                        setRating={setRating}
-                    />
+            <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY} libraries={["places", "geometry", "drawing"]}>
+                <CssBaseline />
+                <Header onLoad={onLoad} onPlaceChanged={onPlaceChanged} />
+                <Grid container spacing={3} style={{ width: '100%' }}>
+                    <Grid item xs={12} md={4}>
+                        <List
+                            places={filteredPlaces.length ? filteredPlaces : places}
+                            childClicked={childClicked}
+                            isLoading={isLoading}
+                            type={type}
+                            setType={setType}
+                            rating={rating}
+                            setRating={setRating}
+                        />
+                    </Grid>
+                    <Grid item xs={12} md={8}>
+                        <Map
+                            setCoordinates={setCoordinates}
+                            setBounds={setBounds}
+                            coordinates={coordinates}
+                            places={filteredPlaces.length ? filteredPlaces : places}
+                            setChildClicked={setChildClicked}
+                            weatherData={weatherData}
+                        />
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} md={8}>
-                    <Map
-                        setCoordinates={setCoordinates}
-                        setBounds={setBounds}
-                        coordinates={coordinates}
-                        places={filteredPlaces.length ? filteredPlaces : places}
-                        setChildClicked={setChildClicked}
-                        weatherData={weatherData}
-                    />
-                </Grid>
-            </Grid>
+            </LoadScript>
         </React.Fragment>
     );
 };
